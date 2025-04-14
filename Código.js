e2ec79514d265465f788da5b33945e7432346ae8
fbelam <fabricio.novaes@hotmai.com>
2025-04-14 13:42:38 -0300

    feature add no sheets

--- Código.js
+++ Código.js
@@ -5,7 +5,36 @@ function doGet() {
 // Processa o formulário de dados simples (nome, datas, etc.)
 function processarFormulario(dados) {
   Logger.log(dados);  // Para visualização no log do Apps Script
-  return `Obrigado, ${dados.cliente}! Sua mensagem foi recebida.`;
+  
+  const planilha = SpreadsheetApp.openById("1xtoZVnFBdAgp39E7pmAUyIOy3uQJ7CY0Tp24Cuw32E4");
+  const aba = planilha.getSheetByName("Reembolso") || planilha.insertSheet("Reembolsos");
+  
+  // Cria a aba se não existir 
+  if (!aba) {
+    aba = planilha.insertSheet("Reembolso")
+    aba.appendRow(["Data envio", "Cliente", "Técnico", "Data Saída", "Data Retorno",
+      "Alimentação", "Mercado", "Combustível", "Táxi", "Estacionamento", "Outros",
+      "Soma Geral", "Link Nota", "Status"]);
+  }
+  const novaLinha = [
+    new Date(), // Data de envio
+    dados.cliente || "",
+    dados.tecnico || "",
+    dados.data_saida || "",
+    dados.data_retorno || "",
+    dados.Alimentação || "0.00",
+    dados.Mercado || "0.00",
+    dados.Combustivel || "0.00",
+    dados.Taxi || "0.00",
+    dados.Estacionamento || "0.00",
+    dados.Outros || "0.00",
+    dados.soma_geral || "0.00",
+    "", // Link da nota fiscal (vamos tratar depois)
+    "Pendente"
+  ];
+  aba.appendRow(novaLinha)
+
+  return `Obrigado, ${dados.tecnico}! Seus dados foram salvos com sucesso.`;
 }
 
 // Recebe arquivo como Base64 e salva no Drive
